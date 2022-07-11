be21b86bb3f26f10cf59f1a9a1756b2571508a63
CristianMP04 <christian.marti.pernin@gmail.com>
2022-07-11 15:50:44 +0200

    add images
    add images

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,20 @@
-// src/App.js
+import ironhackLogo from "/images/ironhack-logo-xs.png";
+import menu from "/images/menu-top-xs.png";
+import icon1 from "/images/icon1.png";
+import icon2 from "/images/icon2.png";
+import icon3 from "/images/icon3.png";
+import icon4 from "/images/icon4.png";
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <header>
+          <img src={ironhackLogo} />
+      </header>
+    </div>
   );
 }
 export default App;
