4c7ba7bb8559a4f9dea27a601b6d8d8cc6e2b6eb
CristianMP04 <christian.marti.pernin@gmail.com>
2022-07-11 16:32:11 +0200

    add less css
    add less css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,33 @@
-import ironhackLogo from "/images/ironhack-logo-xs.png";
-import menu from "/images/menu-top-xs.png";
-import icon1 from "/images/icon1.png";
-import icon2 from "/images/icon2.png";
-import icon3 from "/images/icon3.png";
-import icon4 from "/images/icon4.png";
-
-
 import './App.css';
+import ironhackLogo from "./images/ironhack-logo-xs.png";
+import menu from "./images/menu-top-xs.png";
+import icon1 from "./images/icon1.png";
+import icon2 from "./images/icon2.png";
+import icon3 from "./images/icon3.png";
+import icon4 from "./images/icon4.png";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header>
-          <img src={ironhackLogo} />
+    <div className="container">
+        
+      <header className="App-header">
+        <div class="flexbox-container">
+          <div>
+            <img src={ironhackLogo} alt="#" />
+          </div>
+          <div>
+            <img src={menu} alt="#" />
+          </div>
+            
+        </div>
+          
       </header>
+    
     </div>
+    
   );
 }
 export default App;
